48429fab113fa10a6da0e573d1a4615333bf7f3a
Pooh <forn.k1275@gmail.com>
2023-09-17 15:50:15 +0700

    add picture

--- frontend/src/pages/delivery/components/OrderBar.tsx
+++ frontend/src/pages/delivery/components/OrderBar.tsx
@@ -1,7 +1,9 @@
+import meat_01 from "./orderImages/meat_01.jpg"
+
 function OrderBar() {
     return(
         <div className="order-image">
-            <img src="SoyJuImages/meat_01.jpg" alt="order01" className='meat' />
+            <img src={meat_01} alt="order01" className='meat' />
             <div className="order-text">
                 order small detail 
             </div>
