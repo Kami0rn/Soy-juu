8de0f13d435b4d2292263095b73defd6fdd68af6
Pooh <forn.k1275@gmail.com>
2023-09-17 11:59:17 +0700

    add pages

---
+++ frontend/src/pages/delivery/components/OrderBar.tsx
@@ -0,0 +1,13 @@
+function OrderBar() {
+    return(
+        <div className="order-image">
+            <img src="SoyJuImages/meat_01.jpg" alt="order01" className='meat' />
+            <div className="order-text">
+                order small detail 
+            </div>
+          </div>
+
+    );
+}
+
+export default OrderBar
